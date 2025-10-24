93ed30754ef6fd97d16687ce242b6d3f444ea310
hoshiizumiya <Hoshiizumiya@outlook.com>
2025-10-24 13:17:21 +0800

    Add project files.

---
+++ WinUI3-cppWinRT-consumeSample/MainWindow.xaml.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::WinUI3_cppWinRT_consumeSample::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+    };
+}
+
+namespace winrt::WinUI3_cppWinRT_consumeSample::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
